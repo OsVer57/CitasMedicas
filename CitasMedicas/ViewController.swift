4fa53410d7a2e6b072c5b116188f345476d87fe7
Oscar Vera <osver57@gmail.com>
2019-12-17 17:41:54 -0600

    Initial Commit

---
+++ CitasMedicas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CitasMedicas
+//
+//  Created by Macbook Pro on 17/12/19.
+//  Copyright © 2019 Macbook Pro Oscar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
